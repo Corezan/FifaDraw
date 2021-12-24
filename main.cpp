6841a4028e9b7e535131becbfb0fd04a213cf6d2
Corezan <korzan.kam@gmail.com>
2021-12-24 21:47:24 +0100

    zmiana trybu wyswietlania

--- main.cpp
+++ main.cpp
@@ -2,15 +2,26 @@
 #include <stdlib.h> // niezbêdne dla funkcji srand i rand
 #include <time.h> // niezbêdne dla funkcji time
 #include <iomanip>
-#include <windows.h> // niezbedne do funkcji sleep
+#include <windows.h> // niezbedne do funkcji sleep i ustawienia kursora
+#include <cstdio> // do tabel?
 
 
 using namespace std;
 
+void setCursor(int x, int y)
+{
+     COORD c;
+     c.X = x;
+     c.Y = y;
+     SetConsoleCursorPosition (GetStdHandle (STD_OUTPUT_HANDLE), c);
+}
+
+
 string DM [9];
 string DK [9];
 int i;
-int t= 1500; // ustaw wartosc czas miedzy losowaniami w ms.
+int t= 15; // ustaw wartosc czas miedzy losowaniami w ms.
+bool isDevelopment = true;
 
 int main()
 
@@ -20,27 +31,45 @@ int main()
     {
        srand(time(NULL));
     }
+    if (isDevelopment) {
+        DM[1] = "Legia";
+        DM[2] = "GKS";
+        DM[3] = "Polonia";
+        DM[4] = "Widzew";
+        DM[5] = "Bogdanka";
+        DM[6] = "Lks";
+        DM[7] = "Szczakowianka";
+        DM[8] = "Swit";
 
-    cout << "Wypisz 8 druzyn Michala" << endl;
-    cout << endl;
+        DK[1] = "SPORTING";
+        DK[2] = "SLAVIA";
+        DK[3] = "BAYERN";
+        DK[4] = "BARCELONA";
+        DK[5] = "REAL";
+        DK[6] = "PSG";
+        DK[7] = "JUVENTUS";
+        DK[8] = "ROMA";
+    } else {
+        cout << "Wypisz 8 druzyn Michala" << endl;
+        cout << endl;
 
-
-    for (i=1; i<=8; i++)
-
-    {
+        for (i=1; i<=8; i++)
+        {
             cin>> DM [i];
+        }
+        cout << endl;
+        cout << endl;
+        cout << "Wypisz 8 druzyny Kamila" << endl;
+
+        cout << endl;
+
+        for (i=1; i<=8; i++)
+
+        {
+            cin>> DK [i];
+        }
     }
-cout << endl;
-    cout << endl;
-    cout << "Wypisz 8 druzyny Kamila" << endl;
 
-    cout << endl;
-
-    for (i=1; i<=8; i++)
-
-    {
-        cin>> DK [i];
-    }
 // wpisalismy nazwy zespolow do 2 tablic
 
    int tablica[ 8 ];
@@ -78,56 +107,95 @@ int tab[ 8 ];
 
     cout<< endl;
 Sleep (t);
+/*
+printf("|%-3s|%-15s|%-15s|%-15s|%-15s|", "No.", "Grupa A", "Grupa B", "Grupa C", "Grupa D");
+cout << endl; Sleep(t);
+printf("|%-3s|%-15s|%-15s|%-15s|%-15s|", "---", "---------------", "---------------", "---------------", "---------------");
+cout << endl; Sleep(t);
+printf("|%-3s|%-15s|%-15s|%-15s|%-15s|", "1.", DM [tablica[0]].data(), DK [tab[2]].data(), DM [tablica[4]].data(), DK [tab[6]].data());
+cout << endl; Sleep(t);
+printf("|%-3s|%-15s|%-15s|%-15s|%-15s|", "2.", DK [tab[0]].data(), DM [tablica[2]].data(), DK [tab[4]].data(), DM [tablica[6]].data());
+cout << endl; Sleep(t);
+printf("|%-3s|%-15s|%-15s|%-15s|%-15s|", "3.", DM [tablica[1]].data(), DK [tab[3]].data(), DM [tablica[5]].data(), DK [tab[7]].data());
+cout << endl; Sleep(t);
+printf("|%-3s|%-15s|%-15s|%-15s|%-15s|", "4.", DK [tab[1]].data(), DM [tablica[3]].data(), DK [tab[5]].data(), DM [tablica[7]].data());
+cout << endl;
+*/
+
+
+setCursor( 1,23 );
+cout<<"-----------------------------------------------------------------------------------------------------------------------";
+setCursor( 1,25 );
+cout<< "Grupa A" ;
+setCursor( 30,25 );
+cout<< "Grupa B" ;
+setCursor( 60,25 );
+cout<< "Grupa C" ;
+setCursor( 90,25 );
+cout<< "Grupa D" ;
 
 
 
-cout<< "Grupa A" <<endl<< endl;
-
-            cout<<"1."<< DM [tablica[0]]<<endl;
+setCursor( 2,27 );
+            cout<<"1."<< DM [tablica[0]];
             Sleep(t);
-            cout<<"2."<< DK [tab[0]]<<endl;
-            Sleep(t);
-               cout<<"3." <<DM [tablica[1]]<<endl;
-               Sleep(t);
-                  cout<<"4."<< DK [tab[1]] <<endl<< endl;
-                  Sleep(t);
-
-
-cout<< "Grupa B" <<endl<< endl;
-
-         cout<<"1."<< DK [tab[2]] <<endl;
-         Sleep (t);
-            cout<<"2."<< DM [tablica[2]] <<endl;
+            setCursor( 30,27 );
+            cout<<"1."<< DK [tab[2]] ;
             Sleep (t);
-               cout<<"3."<< DK [tab[3]] <<endl;
-               Sleep (t);
-                  cout<<"4."<< DM [tablica[3]] <<endl<< endl;
-                  Sleep (t);
-
-                  cout<< "Grupa C" <<endl<< endl;
-
-         cout<<"1." <<DM [tablica[4]] <<endl;
-         Sleep(t);
-            cout<<"2."<< DK [tab[4]] <<endl;
+            setCursor( 60,27 );
+            cout<<"1." <<DM [tablica[4]];
             Sleep(t);
-               cout<<"3."<< DM [tablica[5]] <<endl;
-               Sleep(t);
-                  cout<<"4."<< DK [tab[5]] <<endl<< endl;
-                  Sleep(t);
-
-
-                  cout<< "Grupa E" <<endl<< endl;
-
-         cout<<"1."<< DK [tab[6]] <<endl;
-         Sleep (t);
-            cout<<"2."<< DM [tablica[6]] <<endl;
+            setCursor( 90,27 );
+            cout<<"1."<< DK [tab[6]] ;
             Sleep (t);
-               cout<<"3."<< DK [tab[7]] <<endl;
-               Sleep (t);
-                  cout<<"4."<< DM [tablica[7]] <<endl<< endl;
-                  Sleep (t);
 
+setCursor( 2,28 );
 
+            cout<<"2."<< DK [tab[0]];
+            Sleep(t);
+            setCursor( 30,28 );
+            cout<<"2."<< DM [tablica[2]] ;
+            Sleep (t);
+            setCursor( 60,28 );
+            cout<<"2."<< DK [tab[4]];
+            Sleep(t);
+            setCursor( 90,28 );
+            cout<<"2."<< DM [tablica[6]];
+            Sleep (t);
+
+setCursor( 2,29 );
+
+            cout<<"3." <<DM [tablica[1]];
+            Sleep(t);
+            setCursor( 30,29 );
+            cout<<"3."<< DK [tab[3]];
+            Sleep (t);
+            setCursor( 60,29 );
+            cout<<"3."<< DM [tablica[5]];
+            Sleep(t);
+            setCursor( 90,29 );
+            cout<<"3."<< DK [tab[7]];
+            Sleep (t);
+
+setCursor( 2,30 );
+
+            cout<<"4."<< DK [tab[1]];
+            Sleep(t);
+            setCursor( 30,30 );
+            cout<<"4."<< DM [tablica[3]];
+            Sleep (t);
+            setCursor( 60,30 );
+            cout<<"4."<< DK [tab[5]];
+            Sleep(t);
+            setCursor( 90,30 );
+            cout<<"4."<< DM [tablica[7]];
+            Sleep (t);
+
+cout<<endl<<endl;
+
+cout<<"-----------------------------------------------------------------------------------------------------------------------";
+
+cout<<endl<<endl<<endl;
 
 
 
